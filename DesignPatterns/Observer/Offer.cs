2c5a3248fa1070d7c31c92899b703f96d8c92b9d
Ahmed ramadan <Ahmedr2304@gmail.com>
2025-02-21 20:30:33 +0300

    Create a new Class for Offer

---
+++ DesignPatterns/Observer/Offer.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignPatterns.Observer
+{
+    internal class Offer
+    {
+        public string _message { get; set; }
+
+        public Offer(string message)
+        {
+            this._message = message;
+                
+        }
+    }
+}

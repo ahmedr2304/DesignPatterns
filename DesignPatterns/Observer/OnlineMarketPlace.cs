a4f9d24e83193b930d1f45d8268863f5971adb42
Ahmed ramadan <Ahmedr2304@gmail.com>
2025-02-21 20:23:06 +0300

    Create new Class for User

---
+++ DesignPatterns/Observer/OnlineMarketPlace.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Observer
+{
+    internal class OnlineMarketPlace
+    {
+        private List<User> Users;
+        
+        private List<Product> Products;
+
+        private List<Offer> Offers;
+
+    }
+}
---
+++ DesignPatterns/Observer/User.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPatterns.Observer
+{
+    internal class User
+    {
+        public string  _name { get; set; }
+        public bool _isSubscribedToProducts { get; set; }
+        public bool _isSubscribedToOffers { get; set; }
+
+        public User( string name ,bool isSubscribedToProducts,bool isSubscribedToOffers) 
+        {
+            this._name = name;
+            this._isSubscribedToProducts = isSubscribedToProducts;
+            this._isSubscribedToOffers = isSubscribedToOffers;
+
+        }   
+    }
+}
